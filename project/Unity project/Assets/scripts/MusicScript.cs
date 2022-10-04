b9a0e11c455f53f0683847e2d2ea50a9d9719857
Jasmine May Darman <65880499+Linocut@users.noreply.github.com>
2022-10-04 18:28:13 -0400

    WebGl

--- project/Unity project/Assets/scripts/MusicScript.cs
+++ project/Unity project/Assets/scripts/MusicScript.cs
@@ -10,42 +10,81 @@ public class MusicScript : MonoBehaviour
 
     public AudioSource musicSource;
 
-    // Update is called once per frame
-    void Update()
+    Animator anim;
+
+    void Start()
+
     {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
         if (Input.GetKeyDown(KeyCode.W))
+
         {
+
             musicSource.clip = musicClipOne;
+
             musicSource.Play();
+
+            anim.SetInteger("state", 1);
 
         }
 
         if (Input.GetKeyUp(KeyCode.W))
+
         {
+
             musicSource.Stop();
+
+            anim.SetInteger("state", 0);
 
         }
 
         if (Input.GetKeyDown(KeyCode.R))
+
         {
+
             musicSource.clip = musicClipTwo;
+
             musicSource.Play();
+
+            anim.SetInteger("state", 2);
+
         }
 
         if (Input.GetKeyUp(KeyCode.R))
+
         {
+
             musicSource.Stop();
+
+            anim.SetInteger("state", 0);
 
         }
 
         if (Input.GetKeyDown(KeyCode.L))
+
         {
+
             musicSource.loop = true;
+
         }
 
         if (Input.GetKeyUp(KeyCode.L))
+
         {
+
             musicSource.loop = false;
+
         }
+
     }
 }
